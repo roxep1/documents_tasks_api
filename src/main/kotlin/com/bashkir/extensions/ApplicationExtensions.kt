0f26e10a5c66028ab0cf32c81464ecb8dbb45b44
Philipp <isip_f.v.bashkir@mpt.ru>
2022-02-14 11:12:09 +0300

    fix

--- src/main/kotlin/com/bashkir/extensions/ApplicationExtensions.kt
+++ src/main/kotlin/com/bashkir/extensions/ApplicationExtensions.kt
@@ -6,12 +6,13 @@ import io.ktor.application.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.koin.core.logger.Level
 import org.koin.ktor.ext.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureKoin() {
     install(Koin) {
-        slf4jLogger()
+        slf4jLogger(level = Level.ERROR)
         modules(serviceModule)
     }
 }
@@ -23,9 +24,12 @@ fun connectDatabase(user: String, password: String) {
     )
 }
 
-fun createTables() = transaction{
-    exec("CREATE TYPE AgreementStatus AS ENUM ('Sent', 'Agreed', 'Declined');")
-    exec("CREATE TYPE PerformStatus AS ENUM ('Waiting', 'InProgress', 'Completed');")
+fun createTables(enums: Boolean = false) = transaction {
+    if(enums){
+        exec("CREATE TYPE AgreementStatus AS ENUM ('Sent', 'Agreed', 'Declined');")
+        exec("CREATE TYPE PerformStatus AS ENUM ('Waiting', 'InProgress', 'Completed');")
+    }
+
     SchemaUtils.createMissingTablesAndColumns(
         AgreementTable,
         DocumentTable,
