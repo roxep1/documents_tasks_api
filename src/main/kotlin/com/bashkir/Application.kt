db826f6e68e0b2b3f0da82402d5c560eb3e437f2
Philipp <isip_f.v.bashkir@mpt.ru>
2022-02-14 10:13:49 +0300

    database connection

--- src/main/kotlin/com/bashkir/Application.kt
+++ src/main/kotlin/com/bashkir/Application.kt
@@ -1,6 +1,8 @@
 package com.bashkir
 
 import com.bashkir.extensions.configureKoin
+import com.bashkir.extensions.connectDatabase
+import com.bashkir.extensions.createTables
 import com.bashkir.plugins.configureRouting
 import com.bashkir.plugins.configureSerialization
 import io.ktor.application.*
@@ -11,6 +13,8 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    connectDatabase(user = "", password = "") /* База должна называться documents_tasks */
+    createTables()
     configureRouting()
     configureSerialization()
     configureKoin()
--- src/main/kotlin/com/bashkir/extensions/ApplicationExtensions.kt
+++ src/main/kotlin/com/bashkir/extensions/ApplicationExtensions.kt
@@ -1,7 +1,11 @@
 package com.bashkir.extensions
 
+import com.bashkir.models.*
 import com.bashkir.serviceModule
 import io.ktor.application.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.koin.ktor.ext.Koin
 import org.koin.logger.slf4jLogger
 
@@ -10,4 +14,24 @@ fun Application.configureKoin() {
         slf4jLogger()
         modules(serviceModule)
     }
+}
+
+fun connectDatabase(user: String, password: String) {
+    Database.connect(
+        "jdbc:postgresql://localhost:5432/documents_tasks", driver = "org.postgresql.Driver",
+        user = user, password = password
+    )
+}
+
+fun createTables() = transaction{
+    SchemaUtils.createMissingTablesAndColumns(
+        AgreementTable,
+        DocumentTable,
+        FamiliarizeTable,
+        PerformTable,
+        RoleTable,
+        TaskTable,
+        TemplateTable,
+        UserTable
+    )
 }
