52e0ef0f648fc0525a52797e3875b2c8c030c81d
Philipp <isip_f.v.bashkir@mpt.ru>
2022-02-16 21:14:57 +0300

    fix

--- src/main/kotlin/com/bashkir/services/UserService.kt
+++ src/main/kotlin/com/bashkir/services/UserService.kt
@@ -14,8 +14,9 @@ class UserService {
 
     fun getAllUsers(): List<User.Model> = transaction { User.all().map { it.toModel() } }
 
-    fun getTasksToDo(id: String): List<Task.Model> = transaction {
-        User[id].tasksToDo.map { it.task.toModel() }
+    fun getTasksToDo(id: String): List<*> = transaction {
+        val user = User[id]
+        user.tasksToDo.toList().plus(user.agreements).plus(user.familiarizes)
     }
 
     fun getGivenTasks(id: String): List<Task.Model> =
@@ -23,7 +24,8 @@ class UserService {
             User[id].givenTasks.map { it.toModel() }
         }
 
-    fun getAllUserTasks(id: String): List<Task.Model> = getTasksToDo(id).plus(getGivenTasks(id))
+    fun getAllUserTasks(id: String): List<Task.Model> = getGivenTasks(id)
+//        .plus(getTasksToDo(id))
 
     fun getFamiliarizes(id: String): List<Familiarize.Model> =
         transaction { User[id].familiarizes.map { it.toModel() } }
